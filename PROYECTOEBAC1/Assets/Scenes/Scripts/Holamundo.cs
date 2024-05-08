bd417826e91b60653d848f390ca566044ce04a9c
osmore10 <osmoregames@gmail.com>
2024-05-08 11:50:51 -0600

    Hola mundo
    nuevo cambio

---
+++ PROYECTOEBAC1/Assets/Scenes/Scripts/Holamundo.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scripts : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("Algo paso");
+        Debug.Log("Algo paso");
+        Debug.LogWarning("Algo salio mal");
+        Debug.LogError("Algo salio mal");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
